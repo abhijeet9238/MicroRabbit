b53333f8bbbbfcf6411c0968cd85ed66af5012ea
abhijeet9238 <63091634+abhijeet9238@users.noreply.github.com>
2020-07-06 14:41:10 +0530

    Domain core Entities Added

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,16 @@
+﻿using MicroRabbit.Domain.Core.Commands;
+using MicroRabbit.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicroRabbit.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+        Task SendCommand<T>(T command) where T : Command;
+        void Publsh<T>(T @event) where T : Event;
+        void Subsribe<T, THandler>() where T : Event where THandler : IEventHandler<T>; 
+    }
+}
